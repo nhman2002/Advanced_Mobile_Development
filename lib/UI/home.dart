8f93109e7041b60af9e0a88cf071d552c73939e5
PhilipsTruong <phuctinvg9999@gmail.com>
2024-03-07 21:59:18 +0700

    Homescreen

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -1,33 +1,77 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( HomeScreen());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+      routes: {
+        '/login': (context) => LoginPage(),
+      },
+    );
+  }
 }
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-          home: Scaffold(
-              appBar: AppBar(
-                  backgroundColor: Colors.blue,
-                  title: Text('Student Hub'),
-                  actions: [
-                      IconButton(
-                          onPressed: () {
-                              Navigator.of(context).pushNamed('/login');
-                          },
-                          icon: Icon(
-                            Icons.account_circle,
-                            color: Colors.white,
-                            size: 40.0,
-                          ),                          
-                      ),
-                  ],
-              ),
-              body: Center(
-                  child: Text('Hello World!'),
-              ),
-          )
-      );
-  }}
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Student Hub'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed('/login');
+            },
+            icon: Icon(
+              Icons.account_circle,
+              color: Colors.white,
+              size: 40.0,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Xử lý sự kiện khi nhấn nút Company
+                print('Company button pressed');
+              },
+              child: Text('Company'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Xử lý sự kiện khi nhấn nút Student
+                print('Student button pressed');
+              },
+              child: Text('Student'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Text('Login Page'),
+      ),
+    );
+  }
+}
