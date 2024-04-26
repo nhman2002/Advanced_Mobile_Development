096f2927508a4daf30e95cbf03dbf6f72dabe303
Tony Nguyen <75150592+nhman2002@users.noreply.github.com>
2024-04-27 02:04:11 +0700

    update new Page on flow 6 and 7

---
+++ lib/UI/WorkingProjects.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:student_hub/common/config/router.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:student_hub/common/ui/bottomNavigation/AnimatedButton.dart';
+
+class WorkingProjects extends StatefulWidget {
+  const WorkingProjects({super.key});
+
+  @override
+  State<WorkingProjects> createState() => _WorkingProjects();
+}
+
+class _WorkingProjects extends State<WorkingProjects> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Student Hub'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                // Add onPressed logic here
+              },
+              icon: Icon(
+                Icons.account_circle,
+                color: Colors.white,
+                size: 40.0,
+              ),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Your projects',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _buildProjectSectionButton('All', Colors.black, false),
+                  _buildProjectSectionButton('Working', Colors.white, true),
+                  _buildProjectSectionButton('Archived', Colors.black, false),
+                ],
+              ),
+              SizedBox(height: 20),
+              
+              SizedBox(height: 20),
+              Container(
+                height: 450,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: SingleChildScrollView(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Submitted Proposal (0)",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    _buildContent(),
+                    _buildContent(),
+                    _buildContent(),
+                    _buildContent(),
+                    _buildContent(),
+                    _buildContent(),
+                  ],
+                )),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.blue,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(0),
+              child: Row(
+                children: [
+                  _buildCompanyDashboardItem(Icons.work, 'Projects',
+                      Colors.white, Colors.blue.shade300),
+                  _buildCompanyDashboardItem(Icons.dashboard, 'Dashboard',
+                      Colors.black, Colors.grey.shade300),
+                  _buildCompanyDashboardItem(Icons.message, 'Message',
+                      Colors.white, Colors.blue.shade300),
+                  _buildCompanyDashboardItem(Icons.notifications, 'Alerts',
+                      Colors.white, Colors.blue.shade300),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Senior frontend developer (Fintech)',
+            style: TextStyle(color: Colors.green),
+          ),
+          Text(
+            'submitted 3 days ago',
+            style: TextStyle(color: Colors.grey),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '\u2022 ',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                SizedBox(width: 5),
+                Expanded(
+                  child: Text(
+                    "Clear expectation about your project or deliverables",
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(
+            color: Colors.grey,
+            thickness: 1.0,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProjectSectionButton(String label, Color color, bool isBlue) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: isBlue ? Colors.blue : Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: EdgeInsets.all(10),
+        child: Text(
+          label,
+          style: TextStyle(color: color),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCompanyDashboardItem(
+      IconData icon, String label, Color color, Color bgColor) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+      child: AnimatedButton(
+        icon: icon,
+        label: label,
+        color: color,
+        bgColor: bgColor,
+        onPressed: () {
+          // Add onPressed logic here
+          if (label == 'Projects') {
+            context.router.push(const StudentSignupRoute());
+          } else if (label == 'Dashboard') {
+            context.router.push(const StudentSignupRoute());
+          } else if (label == 'Message') {
+            context.router.push(const StudentSignupRoute());
+          } else if (label == 'Alerts') {
+            context.router.push(const StudentSignupRoute());
+          }
+        },
+      ),
+    );
+  }
+}
--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
+import 'package:flutter/widgets.dart';
 import 'package:student_hub/common/config/router.dart';
 import 'package:auto_route/auto_route.dart';
-// import "package:student_hub/UI/Auth/login/login.dart";
-// import "package:student_hub/UI/Auth/company/company.dart";
-// import "package:student_hub/UI/Auth/student/student.dart";
+import 'package:student_hub/common/ui/bottomNavigation/AnimatedButton.dart';
 
 void main() {
   runApp(MyApp());
@@ -40,98 +38,67 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 20),
-            Text(
-              'Build your product with high-skilled student\n',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          AnimatedBackgroundImage(),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 20),
+                Text(
+                  'Build your product with high-skilled student\n',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                SizedBox(
+                  height: 40,
+                  width: 40,
+                  child: Image.asset(
+                    'lib/common/ui/Assets/icon.png',
+                    // Assuming 'assets/icon.png' is the correct path to your icon file
+                  ),
+                ),
+                Text(
+                  '\nFind and onboard best-skilled student for your product. Student works to gain experience & skills from real-world projects \n',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Xử lý sự kiện khi nhấn nút Company
+                    gotoCompanySignup(context);
+                  },
+                  child: Text('Company'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Xử lý sự kiện khi nhấn nút Student
+                    gotoStudentSignup(context);
+                  },
+                  child: Text('Student'),
+                ),
+                Text(
+                  '\n\nStudentHub is a university marketplace to connect high-skilled student and company on a real-world project',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+                SizedBox(height: 20),
+              ],
             ),
-            Text(
-              '\nFind and onboard best-skilled student for your product. Student works to gain experience & skills from real-world projects \n',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Xử lý sự kiện khi nhấn nút Company
-                gotoCompanySignup(context);
-              },
-              child: Text('Company'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Xử lý sự kiện khi nhấn nút Student
-                gotoStudentSignup(context);
-              },
-              child: Text('Student'),
-            ),
-            Text(
-              '\n\nStudentHub is a university marketplace to connect high-skilled student and company on a real-world project',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            SizedBox(height: 20),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 
-  // void gotoLogin(BuildContext context) {
-  //   Navigator.of(context).push(
-  //     PageRouteBuilder(
-  //       pageBuilder: (_, __, ___) => const LoginPage(),
-  //       transitionsBuilder: (_, animation, __, child) {
-  //         return FadeTransition(
-  //           opacity: animation,
-  //           child: child,
-  //         );
-  //       },
-  //     ),
-  //   );
-  // }
-
-  // void gotoCompanySignup(BuildContext context) {
-  //   Navigator.of(context).push(
-  //     PageRouteBuilder(
-  //       pageBuilder: (_, __, ___) => CompanySignup(),
-  //       transitionsBuilder: (_, animation, __, child) {
-  //         return FadeTransition(
-  //           opacity: animation,
-  //           child: child,
-  //         );
-  //       },
-  //     ),
-  //   );
-  // }
-
-  // void gotoStudentSignup(BuildContext context) {
-  //   Navigator.of(context).push(
-  //     PageRouteBuilder(
-  //       pageBuilder: (_, __, ___) => StudentSignup(),
-  //       transitionsBuilder: (_, animation, __, child) {
-  //         return FadeTransition(
-  //           opacity: animation,
-  //           child: child,
-  //         );
-  //       },
-  //     ),
-  //   );
-  // }
   void gotoLogin(BuildContext context) {
     context.router.push(const LoginPageRoute());
   }
@@ -142,5 +109,59 @@ class HomeScreen extends StatelessWidget {
 
   void gotoStudentSignup(BuildContext context) {
     context.router.push(const StudentSignupRoute());
+  }
+}
+
+class AnimatedBackgroundImage extends StatefulWidget {
+  @override
+  _AnimatedBackgroundImageState createState() =>
+      _AnimatedBackgroundImageState();
+}
+
+class _AnimatedBackgroundImageState extends State<AnimatedBackgroundImage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 1.0, end: 1.2).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: _animation.value,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/common/ui/Assets/background.jpeg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
